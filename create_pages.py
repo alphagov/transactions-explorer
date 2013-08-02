5700e203f2453063cd560919932620a028e2b61a
Max Fliri <mfliri@thoughtworks.com>
2013-08-02 12:26:21 +0100

    Create output folder if missing

--- create_pages.py
+++ create_pages.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 
 import unicodecsv
 import re
@@ -8,6 +9,12 @@ from lib.service import Service
 from lib.slugify import slugify
 
 from jinja2 import Environment, FileSystemLoader
+
+
+def create_if_missing(output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
 
 jinja = Environment(
     loader=FileSystemLoader(searchpath='templates', encoding='utf-8'),
@@ -25,19 +32,22 @@ jinja.filters['period_as_text'] = period_as_text
 jinja.filters['slugify'] = slugify
 
 SERVICES_DATA = 'data/services.csv'
+OUTPUT_DIR = 'output/serviceDetails'
+
+create_if_missing(OUTPUT_DIR)
 
 data = open(SERVICES_DATA)
 reader = unicodecsv.DictReader(data)
 for row in reader:
     service = Service(details=row)
     print service.name
-    
+
     if service.high_volume:
         template = jinja.get_template('service_detail.html')
         page = template.render(service=service)
-        
+
         output_filename = '%s/%s.html' % (
-            'output/serviceDetails/',
+            OUTPUT_DIR,
             slugify( '%s-%s' % (service.abbr, service.name) ),
         )
         output = open(output_filename, 'w')
