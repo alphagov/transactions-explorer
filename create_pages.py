cf34e4983bdc1ebecc0e265e6ba274fcaba2f87f
Gareth <grogers@thoughtworks.com>
2013-08-07 12:31:29 +0100

    Only generate pages if create_page.py is called directly
    -   This will prevent pages getting generated if someone
        imports create pages for any reason.

--- create_pages.py
+++ create_pages.py
@@ -41,36 +41,36 @@ def render(template_name, out, vars):
     with open(output_path, 'w') as output:
         output.write(page.encode('utf8'))
 
+if __name__ == "__main__":
+    for service in high_volume_services:
+        print service.name
 
-for service in high_volume_services:
-    print service.name
-
-    render('service_detail.html',
-           out=service.link,
-           vars={"service": service})
+        render('service_detail.html',
+               out=service.link,
+               vars={"service": service})
 
 
-sort_orders = [
-    ("by-name", lambda service: service.name_of_service),
-    ("by-department", lambda service: service.abbr),
-    ("by-total-cost", lambda service: service.most_recent_kpis['cost']),
-    ("by-cost-per-transaction", lambda service: service.most_recent_kpis['cost_per_number']),
-    ("by-digital-takeup", lambda service: service.most_recent_kpis['takeup']),
-    ("by-transactions-per-year", lambda service: service.most_recent_kpis['volume_num']),
-]
+    sort_orders = [
+        ("by-name", lambda service: service.name_of_service),
+        ("by-department", lambda service: service.abbr),
+        ("by-total-cost", lambda service: service.most_recent_kpis['cost']),
+        ("by-cost-per-transaction", lambda service: service.most_recent_kpis['cost_per_number']),
+        ("by-digital-takeup", lambda service: service.most_recent_kpis['takeup']),
+        ("by-transactions-per-year", lambda service: service.most_recent_kpis['volume_num']),
+    ]
 
-for sort_order, key in sort_orders:
-    for direction in ['ascending', 'descending']:
-        reverse = (direction == 'descending')
-        variables = {
-            'services': sorted_ignoring_empty_values(high_volume_services,
-                                                     key=key, reverse=reverse),
-            'latest_quarter': latest_quarter,
-            'current_sort': {
-                'order': sort_order,
-                'direction': direction
-            },
-        }
-        render('high_volume_services.html',
-               out="high-volume-services/%s/%s.html" % (sort_order, direction),
-               vars=variables)
+    for sort_order, key in sort_orders:
+        for direction in ['ascending', 'descending']:
+            reverse = (direction == 'descending')
+            variables = {
+                'services': sorted_ignoring_empty_values(high_volume_services,
+                                                         key=key, reverse=reverse),
+                'latest_quarter': latest_quarter,
+                'current_sort': {
+                    'order': sort_order,
+                    'direction': direction
+                },
+            }
+            render('high_volume_services.html',
+                   out="high-volume-services/%s/%s.html" % (sort_order, direction),
+                   vars=variables)
