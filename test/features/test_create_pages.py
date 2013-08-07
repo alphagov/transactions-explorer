50adc7fe579d7d90e7209a1acb5850ccfb618e8d
Max Fliri <mfliri@thoughtworks.com>
2013-08-07 16:44:08 +0100

    End to end test for all-services page

--- test/features/test_create_pages.py
+++ test/features/test_create_pages.py
@@ -17,5 +17,11 @@ class test_create_pages(unittest.TestCase):
     def test_about_page(self):
         with Browser() as browser:
             browser.visit("http://0.0.0.0:8000/high-volume-services/by-transactions-per-year/descending.html")
-            assert_that(browser.is_text_present('High-volume services'),
-                        is_(True))
+            assert_that(browser.find_by_css('h1').text, is_('High-volume services'))
+
+    def test_all_services(self):
+        with Browser() as browser:
+            browser.visit("http://0.0.0.0:8000/all-services.html")
+            assert_that(browser.find_by_css('h1').text, is_("All Services"))
+            assert_that(browser.find_by_css('#navigation .current').text, is_("All services"))
+
