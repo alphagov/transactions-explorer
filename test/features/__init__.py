38bf3cc2fd31688c29f2c728068a2231b6ac5d32
Gareth <grogers@thoughtworks.com>
2013-08-07 21:08:30 +0100

    Update readme

--- test/features/__init__.py
+++ test/features/__init__.py
@@ -1,3 +1,4 @@
+from nose.plugins.attrib import attr
 from unittest import TestCase
 from splinter import Browser
 from test.features.support.stub_server import HttpStub
@@ -11,9 +12,11 @@ def teardown_package():
     HttpStub.stop()
 
 
+@attr('feature')
 class BrowserTest(TestCase):
     def setUp(self):
         self.browser = Browser('phantomjs', wait_time=3)
 
     def tearDown(self):
         self.browser.quit()
+
