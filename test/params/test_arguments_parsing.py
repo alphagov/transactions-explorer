557d536aebe40bb115d2f0056aaaddf450ccc157
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-08 13:21:20 +0100

    Test for parsing argument parameters

--- test/params/test_arguments_parsing.py
+++ test/params/test_arguments_parsing.py
@@ -6,7 +6,14 @@ from lib.params import parse_args
 class test_arguments_parsing(unittest.TestCase):
 
     def test_default_secret_and_token_to_data_dir(self):
-        argument = parse_args("")
+        argument = parse_args([])
 
         assert_that(argument.client_secrets, is_('data/client_secrets.json'))
         assert_that(argument.oauth_tokens, is_('data/tokens.dat'))
+
+    def test_parse_secret_and_token_params(self):
+        argument = parse_args(['--client-secrets', '/var/google/secrets.json',
+                              '--oauth-tokens', '/var/google/token.dat'])
+
+        assert_that(argument.client_secrets, is_('/var/google/secrets.json'))
+        assert_that(argument.oauth_tokens, is_('/var/google/token.dat'))
