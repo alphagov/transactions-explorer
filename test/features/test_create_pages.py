af5877cca3060048641e0025d573815cec44bd64
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-09 16:46:24 +0100

    Feature tests for sorting all service tables

--- test/features/test_create_pages.py
+++ test/features/test_create_pages.py
@@ -30,3 +30,54 @@ class test_create_pages(BrowserTest):
         self.browser.visit("http://0.0.0.0:8000/all-services")
         assert_that(self.browser.find_by_css('h1').text, is_("All Services"))
         assert_that(self.browser.find_by_css('#navigation .current').text, is_("All services"))
+
+    @nottest
+    def test_sort_by_department_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-department/descending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Attorney General's Office"))
+
+    @nottest
+    def test_sort_by_department_ascending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-department/ascending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Ministry of Justice"))
+
+    @nottest
+    def test_sort_by_digit_take_up_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-takeup/descending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Cabinet Office"))
+
+    @nottest
+    def test_sort_by_digit_take_up_ascending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-takeup/ascending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Department for Work and Pensions"))
+
+    @nottest
+    def test_sort_by_total_cost_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-cost/descending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Department for Work and Pensions"))
+
+    @nottest
+    def test_sort_by_total_cost_ascending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-cost/ascending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Cabinet Office"))
+
+    @nottest
+    def test_sort_by_data_coverage_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-datacoverage/descending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Home Office"))
+
+    @nottest
+    def test_sort_by_data_coverage_ascending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-datacoverage/ascending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"HM Revenue and Customs"))
+
+    @nottest
+    def test_sort_by_volume_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-volume/descending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"HM Revenue and Customs"))
+
+    @nottest
+    def test_sort_by_volume_ascending(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-datacoverage/ascending")
+        assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Department for International Development"))
+        
