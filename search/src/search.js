ee131cbbea2d87f45b192d9591d86175f3b757fd
Gareth <grogers@thoughtworks.com>
2013-08-12 13:41:27 +0100

    Add method for ranking scores and results table

--- search/src/search.js
+++ search/src/search.js
@@ -9,6 +9,32 @@ GOVUK.transactionsExplorer.loadSearchData = function(dataUrl, callback) {
     }).done(callback);
 };
 
+GOVUK.transactionsExplorer.scoreService = (function () {
+    var SEARCH_FIELDS = [
+        "agencyOrBodyAbbreviation",
+        "service",
+        "departmentAbbreviation",
+        "agencyOrBody",
+        "department"
+    ];
+
+    var scoreService = function (searchTerm, service) {
+        var score = 0;
+        $.each(SEARCH_FIELDS, function (i, field) {
+            var valueToSearch = service[field].toLowerCase(),
+                termToUse = searchTerm.toLowerCase();
+
+            if (valueToSearch.search(termToUse) >= 0) {
+                //FIXME this is pretty inefficient but $ doesn't provide a reduce
+                score = service.transactionsPerYear || 1;
+            }
+        });
+        return score;
+    };
+
+    return scoreService;
+}());
+
 GOVUK.transactionsExplorer.search = (function () {
     var data, loaded;
 
@@ -56,4 +82,35 @@ GOVUK.transactionsExplorer.wireSearchForm = function(ids, search) {
         search.performSearch(searchBox.val());
     });
 };
+
+GOVUK.transactionsExplorer.searchResultsTable = (function () {
+    var table = undefined;
+
+    var wireTable = function (id) {
+        table = $(id);
+    };
+
+    var update = function (services) {
+        var rows = [],
+            ROW_CONTENTS = ['service', 'agencyOrBodyAbbreviation', 'category', 'transactionLink', 'transactionsPerYear'];
+
+        $.each(services, function (i, service) {
+            var row = '';
+            $.each(ROW_CONTENTS, function (i, rowKey) {
+                if (i === 0) { 
+                    row += '<th>' + service[rowKey] + '</th>';
+                } else {
+                    row += '<td>' + service[rowKey] + '</td>';
+                }
+            });
+            rows.push('<tr>' + row + '</tr>');
+        });
+        table.find('tbody').html(rows.join(''));
+    };
+
+    return {
+        wireTable: wireTable,
+        update: update
+    };
+}());
 
--- search/test/searchSpec.js
+++ search/test/searchSpec.js
@@ -46,16 +46,139 @@ describe("Searching for services on the transaction explorer. ", function () {
         });
     });
 
+    describe("Result ranking", function () {
+        describe("score:", function () {
+            it('should score a search result with the number of transactions if the service contains the search term', function () {
+                var score = GOVUK.transactionsExplorer.scoreService(
+                    'wibble',
+                    {
+                        agencyOrBodyAbbreviation: "DOW",
+                        service: "Request to wibble",
+                        departmentAbbreviation: "DOW",
+                        agencyOrBody: "",
+                        transactionsPerYear: 4500,
+                        department: "Department of Wibble"
+                    }
+                );
+
+                expect(score).toBe(4500);
+            });
+
+            it('should return a score of 0 if the service does not match the search term', function () {
+                var score = GOVUK.transactionsExplorer.scoreService(
+                    'mongoose',
+                    {
+                        agencyOrBodyAbbreviation: "DOW",
+                        service: "Request to wibble",
+                        departmentAbbreviation: "DOW",
+                        agencyOrBody: "",
+                        transactionsPerYear: 4500,
+                        department: "Department of Wibble"
+                    }
+                );
+
+                expect(score).toBe(0);
+            });
+
+            it('should return a score of 1 if the service matches the search term but does not have any transactions', function () {
+                var score = GOVUK.transactionsExplorer.scoreService(
+                    'Ninjas',
+                    {
+                        agencyOrBodyAbbreviation: "DON",
+                        service: "Dial a ninja",
+                        departmentAbbreviation: "DON",
+                        agencyOrBody: "",
+                        transactionsPerYear: null,
+                        department: "Department of Ninjas"
+                    }
+                );
+
+                expect(score).toBe(1);
+            });
+
+            it('should ignore case when ranking results', function () {
+                var score = GOVUK.transactionsExplorer.scoreService(
+                    'NINJAS',
+                    {
+                        agencyOrBodyAbbreviation: "DON",
+                        service: "Dial a ninja",
+                        departmentAbbreviation: "DON",
+                        agencyOrBody: "",
+                        transactionsPerYear: 7777,
+                        department: "Department of Ninjas"
+                    }
+                );
+
+                expect(score).toBe(7777);
+            });
+        });
+    });
+
     describe("Search", function () {
+        var fakeAjax = undefined;
+
+        beforeEach(function () {
+            fakeAjax = spyOn($, 'ajax').andReturn({
+            done: function (callback) {
+                // no-op
+            }});
+        });
+
         describe("loading search data", function () {
             it('should ajax in some data when loaded', function () {
-                var fakeAjax = spyOn($, 'ajax').andReturn({ done: function () {}});
                 GOVUK.transactionsExplorer.loadSearchData();
                 expect(fakeAjax).toHaveBeenCalled();
             });
         });
 
-        describe("scoring search data", function () {
+        describe("Results box", function () {
+            beforeEach(function () {
+                $('<table id="results"><tbody></tbody></table>').appendTo('body');
+            });
+
+            afterEach(function () {
+                $('#results').remove();
+            });
+
+            it('should update the table with results', function () {
+                var resultsTable = $('#results');
+                GOVUK.transactionsExplorer.searchResultsTable.wireTable('#results');
+                
+                GOVUK.transactionsExplorer.searchResultsTable.update([{
+                        agencyOrBodyAbbreviation: "AA",
+                        service: "some service",
+                        departmentAbbreviation: "DA",
+                        agencyOrBody: "",
+                        transactionsPerYear: 9999,
+                        department: "some department",
+                        category: "some category",
+                        transactionLink: "temp link"
+                }]);
+
+                expect(resultsTable.find('th').first().text()).toBe('some service');
+                expect(resultsTable.find('tr td').first().text()).toBe('AA');
+                expect($(resultsTable.find('tr td').get(1)).text()).toBe('some category');
+                expect($(resultsTable.find('tr td').get(2)).text()).toBe('temp link');
+                expect($(resultsTable.find('tr td').get(3)).text()).toBe('9999');
+            });
+
+            it('should remove old results when updated with new ones', function () {
+                var table = $('table');
+                table.find('tbody').append('<tr id="mr-row"><th>foo</th><td>bar</td></tr>');
+                GOVUK.transactionsExplorer.searchResultsTable.wireTable('#results');
+                GOVUK.transactionsExplorer.searchResultsTable.update({
+                        agencyOrBodyAbbreviation: "AA",
+                        service: "some service",
+                        departmentAbbreviation: "DA",
+                        agencyOrBody: "",
+                        transactionsPerYear: 9999,
+                        department: "some department",
+                        category: "some category",
+                        transactionLink: "temp link"
+                });
+
+                expect($('#mr-row').length).toBe(0);
+            });
         });
     });
 });
