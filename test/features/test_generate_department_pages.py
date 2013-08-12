de22ff8037f5531ed449dc39f6c309da12a4f53e
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-12 15:43:52 +0100

    Display table of transactions

--- test/features/test_generate_department_pages.py
+++ test/features/test_generate_department_pages.py
@@ -2,6 +2,7 @@ from hamcrest import *
 from nose.tools import nottest
 
 from test.features import BrowserTest
+from test.features.support import table_from
 
 
 class GenerateDepartmentPages(BrowserTest):
@@ -24,3 +25,12 @@ class GenerateDepartmentPages(BrowserTest):
         assert_that(self.browser.is_element_not_present_by_css('#data-coverage-notice'),
                     is_(True))
 
+    def test_generate_transactions_table(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc")
+        table = table_from(self.browser.find_by_css('tbody tr'))
+
+        assert_that(table, is_([
+            [u'Service 1', u'ABC', u'Exciting service', u'Access service', u'4,820,000'],
+            [u'Service 2', u'ABC', u'', u'', u'17,150'],
+            [u'Service 3', u'ABC', u'Lame service', u'', u'2,141'],
+        ]))
