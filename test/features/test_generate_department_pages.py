825422763eb1861627bec1c937664d0e4838b800
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-12 14:19:18 +0100

    Feature test for title generation

---
+++ test/features/test_generate_department_pages.py
@@ -0,0 +1,16 @@
+from hamcrest import *
+from nose.tools import nottest
+
+from test.features import BrowserTest
+
+
+class GenerateDepartmentPages(BrowserTest):
+
+    @nottest
+    def test_generates_department_title(self):
+        self.browser.visit("http://0.0.0.0:8000/department/def")
+        title = self.browser.find_by_css('#whitehall-wrapper').text
+
+        assert_that(title, is_(u'Department of Electronic Freedom'))
+
+
