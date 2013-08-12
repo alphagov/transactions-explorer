e481f492d67eba421ebae5689bedc7b4cc2e0542
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-12 15:00:23 +0100

    Feature test for not displaying data coverage
    When there are not high volume services for the department

--- test/features/test_generate_department_pages.py
+++ test/features/test_generate_department_pages.py
@@ -6,15 +6,21 @@ from test.features import BrowserTest
 
 class GenerateDepartmentPages(BrowserTest):
 
-    def test_generates_department_title(self):
+    def test_generates_title(self):
         self.browser.visit("http://0.0.0.0:8000/department/def")
         title = self.browser.find_by_css('#whitehall-wrapper').text
 
         assert_that(title, is_(u'Department of Electronic Freedom'))
 
-    def test_generate_department_data_coverage(self):
+    def test_generate_data_coverage(self):
         self.browser.visit("http://0.0.0.0:8000/department/abc")
         coverage = self.browser.find_by_css('#data-coverage-notice').text
 
         assert_that(coverage, is_(u'Department data coverage: 66.7%\n(taken from 1 high volume services)'))
+
+    def test_do_not_generate_data_coverage_if_no_high_volume_services(self):
+        self.browser.visit("http://0.0.0.0:8000/department/low")
+
+        assert_that(self.browser.is_element_not_present_by_css('#data-coverage-notice'),
+                    is_(True))
 
