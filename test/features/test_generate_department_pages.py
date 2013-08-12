2e0d208df6d68ef6de1eb974dcbe506040c20b1d
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-12 16:04:45 +0100

    Feature test for sorting

--- test/features/test_generate_department_pages.py
+++ test/features/test_generate_department_pages.py
@@ -8,30 +8,61 @@ from test.features.support import table_from
 class GenerateDepartmentPages(BrowserTest):
 
     def test_generates_title(self):
-        self.browser.visit("http://0.0.0.0:8000/department/def")
+        self.browser.visit("http://0.0.0.0:8000/department/def/by-transactions-per-year/descending")
         title = self.browser.find_by_css('#whitehall-wrapper').text
 
         assert_that(title, is_(u'Department of Electronic Freedom'))
 
     def test_generate_data_coverage(self):
-        self.browser.visit("http://0.0.0.0:8000/department/abc")
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-transactions-per-year/descending")
         coverage = self.browser.find_by_css('#data-coverage-notice').text
 
         assert_that(coverage, is_(u'Department data coverage: 66.7%\n(taken from 1 high volume services)'))
 
     def test_do_not_generate_data_coverage_if_no_high_volume_services(self):
-        self.browser.visit("http://0.0.0.0:8000/department/low")
+        self.browser.visit("http://0.0.0.0:8000/department/low/by-transactions-per-year/descending")
 
         assert_that(self.browser.is_element_not_present_by_css('#data-coverage-notice'),
                     is_(True))
 
     def test_generate_transactions_table(self):
-        self.browser.visit("http://0.0.0.0:8000/department/abc")
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-transactions-per-year/descending")
         table = table_from(self.browser.find_by_css('tbody tr'))
 
         assert_that(table, is_([
             [u'Service 1', u'ABC', u'Exciting service', u'Access service', u'4,820,000'],
             [u'Service 2', u'ABC', u'', u'', u'17,150'],
-            [u'Service 3', u'ABC', u'Lame service', u'', u'2,141'],
+            [u'Service 3', u'AAA', u'Lame service', u'', u'2,141'],
         ]))
 
+    def test_first_element_sorted_by_transaction_name_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-name/descending")
+        assert_that(self.browser.find_by_css('tbody tr th').text, is_(u"Service 3"))
+
+    def test_first_element_sorted_by_transaction_name_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-name/ascending")
+        assert_that(self.browser.find_by_css('tbody tr th').text, is_(u"Service 1"))
+
+    def test_first_element_sorted_by_agency_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-agency/descending")
+        assert_that(self.browser.find_by_css('tbody tr th').text, is_(u"Service 1"))
+
+    def test_first_element_sorted_by_transaction_name_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-agency/ascending")
+        assert_that(self.browser.find_by_css('tbody tr th').text, is_(u"Service 1"))
+
+    def test_first_element_sorted_by_category_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-agency/descending")
+        assert_that(self.browser.find_by_css('tbody tr th').text, is_(u"Service 3"))
+
+    def test_first_element_sorted_by_category_ascending(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-agency/ascending")
+        assert_that(self.browser.find_by_css('tbody tr th').text, is_(u"Service 2"))
+
+    def test_first_element_sorted_by_transaction_per_year_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-transactions-per-year/descending")
+        assert_that(self.browser.find_by_css('tbody tr th').text, is_(u"Service 1"))
+
+    def test_first_element_sorted_by_transaction_name_descending(self):
+        self.browser.visit("http://0.0.0.0:8000/department/abc/by-transactions-per-year/descending")
+        assert_that(self.browser.find_by_css('tbody tr th').text, is_(u"Service 3"))
