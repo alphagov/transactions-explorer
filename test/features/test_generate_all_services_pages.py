25337f3c4544b4b341585736267df682f49d841d
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-12 11:49:37 +0100

    Test all services table content

--- test/features/test_generate_all_services_pages.py
+++ test/features/test_generate_all_services_pages.py
@@ -5,44 +5,61 @@ from test.features import BrowserTest
 
 class GenerateAllServicesPages(BrowserTest):
 
+    def table_from(self, rows):
+        def cells(row):
+            name = [row.find_by_css('th').text]
+            values = map(lambda elem: elem.text, row.find_by_css('td'))
+            return name + values
 
-    def test_sort_by_department_descending(self):
+        return map(cells, rows)
+
+
+    def test_all_services_table(self):
+        self.browser.visit("http://0.0.0.0:8000/all-services/by-volume/descending")
+        table = self.table_from(self.browser.find_by_css('tbody tr'))
+
+        assert_that(table, is_([
+            [u'Department of Electronic Freedom', u'84.7%', u'\xa3157m', u'46.7%', u'21,117,614'],
+            [u'Agency for Beautiful Code', u'100%', u'\xa3482k', u'66.7%', u'4,839,291'],
+            [u'Lower Order Workgroups', u'', u'', u'', u'29,127']
+        ]))
+
+    def test_first_element_sort_by_department_descending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-department/descending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Lower Order Workgroups"))
 
-    def test_sort_by_department_ascending(self):
+    def test_first_element_sort_by_department_ascending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-department/ascending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Agency for Beautiful Code"))
 
-    def test_sort_by_digit_take_up_descending(self):
+    def test_first_element_sort_by_digit_take_up_descending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-takeup/descending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Agency for Beautiful Code"))
 
-    def test_sort_by_digit_take_up_ascending(self):
+    def test_first_element_sort_by_digit_take_up_ascending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-takeup/ascending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Department of Electronic Freedom"))
 
-    def test_sort_by_total_cost_descending(self):
+    def test_first_element_sort_by_total_cost_descending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-cost/descending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Department of Electronic Freedom"))
 
-    def test_sort_by_total_cost_ascending(self):
+    def test_first_element_sort_by_total_cost_ascending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-cost/ascending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Agency for Beautiful Code"))
 
-    def test_sort_by_data_coverage_descending(self):
+    def test_first_element_sort_by_data_coverage_descending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-data-coverage/descending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Agency for Beautiful Code"))
 
-    def test_sort_by_data_coverage_ascending(self):
+    def test_first_element_sort_by_data_coverage_ascending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-data-coverage/ascending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Department of Electronic Freedom"))
 
-    def test_sort_by_volume_descending(self):
+    def test_first_element_sort_by_volume_descending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-volume/descending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Department of Electronic Freedom"))
 
-    def test_sort_by_volume_ascending(self):
+    def test_first_element_sort_by_volume_ascending(self):
         self.browser.visit("http://0.0.0.0:8000/all-services/by-volume/ascending")
         assert_that(self.browser.find_by_css('tbody tr th a').text, is_(u"Lower Order Workgroups"))
-
