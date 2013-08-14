ef2c8fcc98851c9090f01a0c36009dada6f5c7b5
Max Fliri <mfliri@thoughtworks.com>
2013-08-14 12:30:06 +0100

    Clear output folder before generating artefact
    This is to avoid including files left from previous builds

--- build_artefact.sh
+++ build_artefact.sh
@@ -5,8 +5,8 @@ if [ -z "$VIRTUAL_ENV" ]; then
   exit 1
 fi
 
-mkdir -p artefacts
-rm -f artefacts/*
+mkdir -p artefacts; rm -f artefacts/*
+mkdir -p output; rm -Rf output/*
 
 if [ -n "${CLIENT_SECRETS}" ]; then FETCH_ARGS="${FETCH_ARGS} --client-secrets ${CLIENT_SECRETS}"; fi
 if [ -n "${OAUTH_TOKENS}" ];   then FETCH_ARGS="${FETCH_ARGS} --oauth-tokens ${OAUTH_TOKENS}"; fi
