1f5ea1b14e7d4ff64facf2812dd397379bbbe429
Max Fliri <mfliri@thoughtworks.com>
2013-08-16 11:20:34 +0100

    Cleanup build scripts

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -euo pipefail
+VENV_NAME=$1
 
-VIRTUALENV_DIR=/var/tmp/virtualenvs/$(echo ${JOB_NAME} | tr ' ' '-')
+VIRTUALENV_DIR="/var/tmp/virtualenvs/$VENV_NAME"
 export PIP_DOWNLOAD_CACHE=/var/tmp/pip_download_cache
 
 virtualenv --clear --no-site-packages $VIRTUALENV_DIR
 source $VIRTUALENV_DIR/bin/activate
 
-./run_tests.sh
-./build-artefact.sh
+pip install -r requirements.txt
