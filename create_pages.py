97f46dbd98b4b3ed669b77f147371ca754ff0d55
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-20 15:43:48 +0100

    Sort abbreviations in lower case.
    Avoids case sensitive ordering issues.

--- create_pages.py
+++ create_pages.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
 
     sort_orders = [
         ("by-name", lambda service: service.name_of_service),
-        ("by-department", lambda service: service.abbr),
+        ("by-department", lambda service: service.abbr.lower()),
         ("by-total-cost", lambda service: service.latest_kpi_for('cost')),
         ("by-cost-per-transaction", lambda service: service.latest_kpi_for('cost_per_number')),
         ("by-digital-takeup", lambda service: service.latest_kpi_for('takeup')),
@@ -96,7 +96,7 @@ if __name__ == "__main__":
 
     services_sort_orders = [
         ("by-name", lambda service: service.name_of_service),
-        ("by-agency", lambda service: service.agency_abbreviation),
+        ("by-agency", lambda service: service.agency_abbreviation.lower()),
         ("by-category", lambda service: service.category),
         ("by-transactions-per-year", lambda service: service.most_up_to_date_volume),
     ]
