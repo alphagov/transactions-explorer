a553c092560616ebfaa5fa60a5c85fcada1abb9b
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-08-28 15:36:50 +0100

    Use getattr rather than direct __dict__ access
    Mirror what we're doing when setting the attributes.

--- lib/service/__init__.py
+++ lib/service/__init__.py
@@ -166,9 +166,8 @@ class Service:
 
         return map(key_data, reversed(previous_kpis))
 
-
     def __getitem__(self, key):
-        return self.__dict__[key]
+        return getattr(self, key)
 
     def _split_keywords(self, details):
         if not details['Keywords']:
