1dfbd18b6eb8654e72ed259b7452bbc09812ab69
Paulo Schneider <paulo.schneider@gmail.com>
2013-09-02 13:52:04 +0100

    Retry generating treemap fallback

--- treemaps_with_pool.py
+++ treemaps_with_pool.py
@@ -1,10 +1,11 @@
 import fnmatch
 import json
-from multiprocessing import pool
+from multiprocessing import pool, Pool
 import os
 import subprocess
 import sys
 import threading
+from time import sleep
 
 matched_files = []
 look_in = [os.path.join('output', 'department'),
@@ -12,17 +13,9 @@ look_in = [os.path.join('output', 'department'),
            os.path.join('output', 'high-volume-services')]
 
 
-def do_timeout(process):
-    if process.poll() is None:
-        try:
-            print "attempting to kill phantom because it looks like it's hanging..."
-            process.kill()
-        except:
-            pass
-
-
 if __name__ == "__main__":
     base_url = sys.argv[1] if len(sys.argv) > 1 else 'http://localhost:8080/'
+    expected_outputs = []
 
     for path in look_in:
         for root, dirnames, filenames in os.walk(path):
@@ -30,22 +23,24 @@ if __name__ == "__main__":
                 ignore, url_fragment = os.path.join(root, filename).replace(
                     '.html', '').split('/', 1)
                 matched_files.append(url_fragment)
+                expected_outputs.append("output/treemaps/%s.html" % url_fragment)
 
     with open('treemaps.json', 'w') as out:
         json.dump(matched_files, out)
 
-    def target():
-        subprocess.call(['phantomjs',
-                         'grab_treemap_html.js',
-                         base_url,
-                         'treemaps.json',
-                         'output'])
+    def all_files_generated():
+        return all(os.path.isfile(file) for file in expected_outputs)
 
-    t = threading.Thread(target=target)
-    t.start()
-    t.join(timeout=5.0)
+    while not all_files_generated():
+        process = subprocess.Popen(['phantomjs',
+                                   'grab_treemap_html.js',
+                                   base_url,
+                                   'treemaps.json',
+                                   'output'])
 
-    if t.is_alive():
-        print "attempting to kill phantom because it looks like it's hanging..."
-        t.kill()
+        sleep(30)
+        if process.poll() is None:
+            print "killing"
+            process.terminate()
 
+    print "Finished"
