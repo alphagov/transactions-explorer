92907eccc76ba5f7be4be5e26bdbf37b4aeca593
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-10-25 11:39:09 +0100

    Use environment specific static manifest on deploy
    This will allow us to spot issues in static before it is deployed
    to production. As it was a few days ago. After a month of no
    deploys *facepalm*

--- build_artefact.sh
+++ build_artefact.sh
@@ -5,11 +5,14 @@ if [ -z "$VIRTUAL_ENV" ]; then
     exit 1
 fi
 
+# Download the environment-specific static manifest
+curl "${ASSET_HOST}/static/manifest.yml" > data/static-digests.yml
+
 mkdir -p artefacts
 mkdir -p output; rm -Rf output/*
 
 if [ -n "${PATH_PREFIX}" ]; then CREATE_ARGS="${CREATE_ARGS} --path-prefix ${PATH_PREFIX}"; fi
-if [ -n "${ASSET_PREFIX}" ]; then CREATE_ARGS="${CREATE_ARGS} --asset-prefix ${ASSET_PREFIX}"; fi
+if [ -n "${ASSET_HOST}" ]; then CREATE_ARGS="${CREATE_ARGS} --asset-prefix ${ASSET_HOST}/transactions-explorer"; fi
 CREATE_ARGS="${CREATE_ARGS} --static-digests data/static-digests.yml"
 
 python create_pages.py $CREATE_ARGS
