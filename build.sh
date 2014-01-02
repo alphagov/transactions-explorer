d8675e9bdcc173a857894f2eab1427e666df71ff
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-01-02 16:48:08 +0000

    Use static assets from correct environment
    Currently we use production CDN assets in every environment. This change
    will let us use preview assets in preview, etc.
    
    Adds a new environment variable CDN_URL. Still defaults to production in
    lib/filters/__init__.py.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "Building from version ${APPLICATION_VERSION}"
 
-export CLIENT_SECRETS="/etc/google/oauth/client_secrets.json" 
+export CLIENT_SECRETS="/etc/google/oauth/client_secrets.json"
 export OAUTH_TOKENS="/var/lib/google/oauth/drive.db"
 export PATH_PREFIX="/performance/transactions-explorer/"
 export TREEMAP_ARTEFACT_NAME="treemaps.tgz"
@@ -12,6 +12,7 @@ export TREEMAP_ARTEFACT_NAME="treemaps.tgz"
 
 ./build_treemaps.sh
 
-ASSET_PREFIX="https://assets-origin.preview.alphagov.co.uk/transactions-explorer" ARTEFACT_NAME="transactions-explorer-preview.tgz" ./build_artefact.sh
-ASSET_PREFIX="https://assets-origin.production.alphagov.co.uk/transactions-explorer" ARTEFACT_NAME="transactions-explorer-staging.tgz" ./build_artefact.sh
-ASSET_PREFIX="https://assets.digital.cabinet-office.gov.uk/transactions-explorer" ARTEFACT_NAME="transactions-explorer-production.tgz" ./build_artefact.sh
+CDN_URL="https://assets-origin.preview.alphagov.co.uk" ARTEFACT_NAME="transactions-explorer-preview.tgz" ./build_artefact.sh
+# Use production CDN in staging because the staging CDN URL is restricted by IP address
+CDN_URL="https://assets.digital.cabinet-office.gov.uk" ARTEFACT_NAME="transactions-explorer-staging.tgz" ./build_artefact.sh
+CDN_URL="https://assets.digital.cabinet-office.gov.uk" ARTEFACT_NAME="transactions-explorer-production.tgz" ./build_artefact.sh
