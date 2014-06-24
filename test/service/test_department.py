a91066253c4afe934c6cba5bcf4be10857a3670a
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-06-24 15:20:10 +0100

    Add Q2 2014 data (April 2013 to March 2014)

--- test/service/test_department.py
+++ test/service/test_department.py
@@ -350,8 +350,8 @@ class TestDepartmentDataCoverage(unittest.TestCase):
 
         coverage = dept.data_coverage
 
-        assert_that(float(coverage.percentage), is_(.25))
-        assert_that(coverage.requested, is_(36))
+        assert_that(float(coverage.percentage), close_to(0.21428, 0.001))
+        assert_that(coverage.requested, is_(42))
         assert_that(coverage.provided, is_(9))
 
     def test_data_coverage_excludes_non_high_volume_services(self):
@@ -376,8 +376,8 @@ class TestDepartmentDataCoverage(unittest.TestCase):
 
         coverage = dept.data_coverage
 
-        assert_that(float(coverage.percentage), close_to(0.1666, 0.001))
-        assert_that(coverage.requested, is_(18))
+        assert_that(float(coverage.percentage), close_to(0.14285, 0.001))
+        assert_that(coverage.requested, is_(21))
         assert_that(coverage.provided, is_(3))
 
     def test_data_coverage_is_none_when_no_high_volume_services(self):
@@ -424,6 +424,6 @@ class TestDepartmentDataCoverage(unittest.TestCase):
 
         coverage = dept.data_coverage
 
-        assert_that(float(coverage.percentage), close_to(0.1818, 0.001))
-        assert_that(coverage.requested, is_(33))
+        assert_that(float(coverage.percentage), close_to(0.15384, 0.001))
+        assert_that(coverage.requested, is_(39))
         assert_that(coverage.provided, is_(6))
